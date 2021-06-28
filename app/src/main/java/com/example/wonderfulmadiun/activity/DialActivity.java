c410a61fe1ff9275dabdd160c8d2c34e08301e96
bimaandriansyah <bimaandrian73@gmail.com>
2021-06-28 11:17:10 +0700

    dial activity

--- app/src/main/java/com/example/wonderfulmadiun/activity/DialActivity.java
+++ app/src/main/java/com/example/wonderfulmadiun/activity/DialActivity.java
@@ -3,14 +3,19 @@ package com.example.wonderfulmadiun.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.wonderfulmadiun.R;
 
 public class DialActivity extends AppCompatActivity {
 
     Toolbar tbDial;
+    LinearLayout btnAmbulan,btnPolisi,btnPemadam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,38 @@ public class DialActivity extends AppCompatActivity {
         setSupportActionBar(tbDial);
         assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnAmbulan = findViewById(R.id.btnPanggilAmbulance);
+        btnPolisi = findViewById(R.id.btnPanggilPolisi);
+        btnPemadam = findViewById(R.id.btnPanggilPemadam);
+
+        btnAmbulan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:119"));
+                startActivity(intent);
+            }
+        });
+
+        btnPemadam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:112"));
+                startActivity(intent);
+            }
+        });
+
+        btnPolisi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:112"));
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
