798534366c62aa73a02404ce0abf792760512b57
Rezzy <rezzyandrean@gmail.com>
2021-07-02 19:57:25 +0700

    REST API Updates

--- app/src/main/java/com/example/wonderfulmadiun/rest/RestApi.java
+++ app/src/main/java/com/example/wonderfulmadiun/rest/RestApi.java
@@ -2,15 +2,15 @@ package com.example.wonderfulmadiun.rest;
 
 public class RestApi {
 
-    public static String Wisata             = "https://wonderfulmadiun.000webhostapp.com/api/wisata/";
-    public static String DetailWisata       = "https://wonderfulmadiun.000webhostapp.com/api/wisata/";
-    public static String Penginapan         = "https://wonderfulmadiun.000webhostapp.com/api/penginapan/";
-    public static String Kuliner            = "https://wonderfulmadiun.000webhostapp.com/api/kuliner/";
-    public static String Coffee             = "https://wonderfulmadiun.000webhostapp.com/api/coffeeshop/";
-    public static String Prayplace          = "https://wonderfulmadiun.000webhostapp.com/api/tempat_ibadah/";
-    public static String DetailPrayplace    = "https://wonderfulmadiun.000webhostapp.com/api/tempat_ibadah/";
-    public static String Perbelanjaan       = "https://wonderfulmadiun.000webhostapp.com/api/perbelanjaan/";
-    public static String DetailPerbelanjaan = "https://wonderfulmadiun.000webhostapp.com/api/perbelanjaan/";
+    public static String Wisata             = "https://wonderful-madiun.herokuapp.com/api/wisata/";
+    public static String DetailWisata       = "https://wonderful-madiun.herokuapp.com/api/wisata/";
+    public static String Penginapan         = "https://wonderful-madiun.herokuapp.com/api/penginapan/";
+    public static String Kuliner            = "https://wonderful-madiun.herokuapp.com/api/kuliner/";
+    public static String Coffee             = "https://wonderful-madiun.herokuapp.com/api/coffeeshop/";
+    public static String Prayplace          = "https://wonderful-madiun.herokuapp.com/api/tempat_ibadah/";
+    public static String DetailPrayplace    = "https://wonderful-madiun.herokuapp.com/api/tempat_ibadah/";
+    public static String Perbelanjaan       = "https://wonderful-madiun.herokuapp.com/api/perbelanjaan/";
+    public static String DetailPerbelanjaan = "https://wonderful-madiun.herokuapp.com/api/perbelanjaan/";
 
 
 }
