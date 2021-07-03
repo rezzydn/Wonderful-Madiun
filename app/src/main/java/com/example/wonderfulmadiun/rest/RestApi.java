67c6f545780bd2ebd2e5c186ede23a52d94f061c
bimaandriansyah <bimaandrian73@gmail.com>
2021-07-03 15:21:04 +0700

    view

--- app/src/main/java/com/example/wonderfulmadiun/rest/RestApi.java
+++ app/src/main/java/com/example/wonderfulmadiun/rest/RestApi.java
@@ -2,15 +2,14 @@ package com.example.wonderfulmadiun.rest;
 
 public class RestApi {
 
-    public static String Wisata             = "https://wonderfulmadiun.000webhostapp.com/api/wisata/";
-    public static String DetailWisata       = "https://wonderfulmadiun.000webhostapp.com/api/wisata/";
-    public static String Penginapan         = "https://wonderfulmadiun.000webhostapp.com/api/penginapan/";
-    public static String Kuliner            = "https://wonderfulmadiun.000webhostapp.com/api/kuliner/";
-    public static String Coffee             = "https://wonderfulmadiun.000webhostapp.com/api/coffeeshop/";
-    public static String Prayplace          = "https://wonderfulmadiun.000webhostapp.com/api/tempat_ibadah/";
-    public static String DetailPrayplace    = "https://wonderfulmadiun.000webhostapp.com/api/tempat_ibadah/";
-    public static String Perbelanjaan       = "https://wonderfulmadiun.000webhostapp.com/api/perbelanjaan/";
-    public static String DetailPerbelanjaan = "https://wonderfulmadiun.000webhostapp.com/api/perbelanjaan/";
+    public static String Wisata             = "http://wonderful-madiun.herokuapp.com//api/wisata/";
+    public static String Penginapan         = "http://wonderful-madiun.herokuapp.com//api/penginapan/";
+    public static String Kuliner            = "http://wonderful-madiun.herokuapp.com//api/kuliner/";
+    public static String Coffee             = "http://wonderful-madiun.herokuapp.com//api/coffeeshop/";
+    public static String Prayplace          = "http://wonderful-madiun.herokuapp.com//api/tempat_ibadah/";
+    public static String DetailPrayplace    = "http://wonderful-madiun.herokuapp.com//api/tempat_ibadah/";
+    public static String Perbelanjaan       = "http://wonderful-madiun.herokuapp.com//api/perbelanjaan/";
+    public static String DetailPerbelanjaan = "http://wonderful-madiun.herokuapp.com//api/perbelanjaan/";
 
 
 }
